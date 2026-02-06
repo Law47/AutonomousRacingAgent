4cbfe609e32a9ce1608618f968f6f0375b1cbcf8
zleodai <awsomeleodai@gmail.com>
2026-02-06 00:00:35 -0800

    stuff

--- Training/train.py
+++ Training/train.py
@@ -1,1 +1,12 @@
+from pathlib import Path
+import sys, os
 import gymnasium as gym
+
+trainingPath = Path(__file__).parent.parent
+modelPath = Path(__file__).parent.parent / 'Model'
+discorPath = modelPath / 'discor'
+sys.path.extend([str(trainingPath), str(modelPath), str(discorPath)])
+
+from discor.algorithm import SAC, DisCor
+from discor.agent import Agent
+
